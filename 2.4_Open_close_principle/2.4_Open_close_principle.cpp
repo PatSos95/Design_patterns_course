a28b924b88218e5ed7bd20dcc7bede1a34ba8ae9
PatSos95 <patsos95@gmail.com>
2021-12-02 13:01:01 +0100

    2.4 Open close principle exercise done

--- 2.4_Open_close_principle/2.4_Open_close_principle.cpp
+++ 2.4_Open_close_principle/2.4_Open_close_principle.cpp
@@ -1,20 +1,101 @@
-// 2.4_Open_close_principle.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+
+enum class Color { red, green, blue };
+enum class Size { small, medium, large };
+
+struct Product
+{
+    std::string name;
+    Color color;
+    Size size;
+};
+
+
+template <typename T>
+struct Specification
+{
+    virtual bool isSatisfied(T* item) = 0;
+};
+
+template <typename T> 
+struct Filter
+{
+    virtual std::vector<T*> filter(std::vector<T*> items, Specification<T>& spec) = 0;
+};
+
+struct FilterImpl : Filter<Product>
+{
+    std::vector<Product*> filter(std::vector<Product*> items, Specification<Product>& spec) override
+    {
+        std::vector<Product*> result;
+        for (auto item : items)
+        {
+            if (spec.isSatisfied(item))
+            {
+                result.push_back(item);
+            }
+        }
+        return result;
+    }
+};
+
+struct ColorSpecification : Specification<Product>
+{
+    Color color;
+    
+    ColorSpecification(Color color) : color(color) {}
+
+    bool isSatisfied(Product* item) override
+    {
+        return item->color == color;
+    }
+};
+
+struct SizeSpecification : Specification<Product>
+{
+    Size size;
+
+    SizeSpecification(Size size) : size(size) {}
+
+    bool isSatisfied(Product* item) override
+    {
+        return item->size == size;
+    }
+};
+
+struct AndSpecification : Specification <Product>
+{
+    Specification<Product>& first;
+    Specification<Product>& second;
+
+    AndSpecification(Specification<Product>& first, Specification<Product>& second) 
+    : first(first), second(second) {}
+
+    bool isSatisfied(Product* item) override
+    {
+        return first.isSatisfied(item) && second.isSatisfied(item);
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Product apple{"Apple", Color::green, Size::small};
+    Product tree{"Tree", Color::green, Size::large};
+    Product house{"House", Color::blue, Size::large};
+
+    std::vector<Product*> items {&apple, &tree, &house};
+
+    FilterImpl filter;
+    ColorSpecification green {Color::green};
+    SizeSpecification large {Size::large};
+    AndSpecification greenAndLarge {green, large};
+
+    std::vector<Product*> FilteredItems = filter.filter(items, greenAndLarge);
+
+    for (auto item : FilteredItems)
+    {
+        std::cout << item->name << ", ";
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
